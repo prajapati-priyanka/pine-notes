61ab51e8156234544eed56f72827fd2302bdac97
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-21 14:19:17 +0530

    feat: Add two buttons

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -8,10 +8,15 @@ const Landing = () =>{
                 
                 <section className="main-description">
                     <h1 className="main-title">Pine <span>Notes</span></h1>
-                    <h2 className="main-sub-title">Tame your work, organise your life.</h2>
+                    <h2 className="main-subtitle">Tame your work, organise your life.</h2>
                     <p className="main-text lg-text">Remember everything and tackle any project with your notes, tasks,and schedule all in one place.</p>
+                    <div className="user-links">
+                    <button className="btn btn-primary md-text">Sign Up</button>
+                    <button className="btn btn-outline-primary md-text">Get Started</button>
                     
-                    <button className="btn btn-primary md-text">Get Started</button>
+                    </div>
+                    <button className="btn-login md-text">Already have an account?</button>  
+                    
                     
                    
                 </section>
