cb1bbc3b33eb8d80270b2a259cea1784effdb944
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-21 16:26:37 +0530

    feat: Change Nav component content

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,38 @@
+import { FaRegUser } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { GiHamburgerMenu} from "react-icons/gi"
 import "./Nav.css"
 
 const Nav = ()=>{
     return(
-        <header className="header">
-            <h2>Pine <span>Notes</span></h2>
-            <nav className="header-links">
-                <button className="btn btn-login lg-text">Login</button>
-                <button className="btn btn-primary md-text">Sign Up</button>
-            </nav>
-        </header>
+    
+        <header className="header-container">
+          <GiHamburgerMenu className="hamburger-menu x-lg-text" />
+          <h2 className="header-title">Pine <span>Notes</span></h2>
+          <form className="search-container">
+            <input
+              type="search"
+              placeholder="Type to search"
+              className="input-search"
+            />
+            <button type="submit" className="btn-search">
+              <FaSearch />
+            </button>
+          </form>
+
+          <nav className="nav-container">
+            <ul className="nav-list md-text">
+              <li className="list-inline-item">
+                <a className="nav-icon-link link-no-style">
+                  <span className="nav-icon lg-text">
+                    <FaRegUser />
+                  </span>
+                  <span className="nav-icon-text md-text"> Hi! User </span>
+                </a>
+              </li>
+              </ul>
+              </nav>
+              </header>
     )
 }
 
