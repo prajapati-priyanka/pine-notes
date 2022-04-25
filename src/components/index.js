1059dc40a56cdfe6873e6312639b26d71df6a6e9
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-25 12:35:13 +0530

    refactor: Insert semicolon

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export {CreateNote} from "./createNote/CreateNote"
+export {CreateNote} from "./createNote/CreateNote";
 export {Filter} from "./filter/Filter";
 export{EditLable} from "./modal/EditLabel";
-export {NoteCard} from "./noteCard/NoteCard"
-export {SideNav} from "./sideNav/SideNav"
+export {NoteCard} from "./noteCard/NoteCard";
+export {SideNav} from "./sideNav/SideNav";
