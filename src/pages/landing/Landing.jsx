66a0ad645d6c3192f7e79b9329236c8b1859e27a
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-25 12:14:30 +0530

    feat: Add Link

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,33 +1,45 @@
-import "./Landing.css"
+import { Link } from "react-router-dom";
+import "./Landing.css";
 
-const Landing = () =>{
-    return(
-        <div>
-            {/* <Nav /> */}
-            <main className="main text-center">
-                
-                <section className="main-description">
-                    <h1 className="main-title">Pine <span>Notes</span></h1>
-                    <h2 className="main-subtitle">Tame your work, organise your life.</h2>
-                    <p className="main-text lg-text">Remember everything and tackle any project with your notes, tasks,and schedule all in one place.</p>
-                    <div className="user-links">
-                    <button className="btn btn-primary md-text">Sign Up</button>
-                    <button className="btn btn-outline-primary md-text">Get Started</button>
-                    
-                    </div>
-                    <button className="btn-login md-text">Already have an account?</button>  
-                    
-                    
-                   
-                </section>
-                <figure className="main-hero">
-                    <img src="../assets/hero.svg" className="img-responsive" alt="hero-img"/>
-                </figure>
-               
-            </main>
-          
-        </div>
-    )
-}
+const Landing = () => {
+  return (
+    <div>
+      <main className="main text-center">
+        <section className="main-description">
+          <h1 className="main-title">
+            Pine <span>Notes</span>
+          </h1>
+          <h2 className="main-subtitle">Tame your work, organise your life.</h2>
+          <p className="main-text lg-text">
+            Remember everything and tackle any project with your notes,
+            tasks,and schedule all in one place.
+          </p>
+          <div className="user-links">
+            <Link to="/signup">
+              <button className="btn btn-primary md-text">Sign Up</button>
+            </Link>
+            <Link to="/home">
+              <button className="btn btn-outline-primary md-text">
+                Get Started
+              </button>
+            </Link>
+          </div>
+          <Link to="/login">
+            <button className="btn-login md-text">
+              Already have an account?
+            </button>
+          </Link>
+        </section>
+        <figure className="main-hero">
+          <img
+            src="../assets/hero.svg"
+            className="img-responsive"
+            alt="hero-img"
+          />
+        </figure>
+      </main>
+    </div>
+  );
+};
 
-export {Landing};
+export { Landing };
