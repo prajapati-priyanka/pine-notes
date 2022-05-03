b01575c37d80a8d687c41fdff50f709e69299440
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-04 01:50:14 +0530

    feat: import PinnedCard

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {Filter} from "./filter/Filter";
 export{EditLable} from "./modal/EditLabel";
 export {NoteCard} from "./noteCard/NoteCard";
 export {SideNav} from "./sideNav/SideNav";
+export {PinnedCard} from "./pinnedCard/PinnedCard";
