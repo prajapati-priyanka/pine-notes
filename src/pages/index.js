810562f6ed1a184bf40660d9297f28c82460c767
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-04 01:53:24 +0530

    feat: import landing and notes page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export {Home} from "./home/Home";
 export {Login} from "./authentication/login/Login";
 export {Signup} from "./authentication/signup/Signup";
-export {Landing} from "./landing/Landing"
+export {Landing} from "./landing/Landing";
+export {Notes} from "./notes/Notes"
