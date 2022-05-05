20742de4090925fd84aad0ecacd2e2af94b33f80
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-05 23:44:33 +0530

    feat: add states to open color pallete

--- src/context/notes-context.js
+++ src/context/notes-context.js
@@ -4,8 +4,6 @@ import { useAuth } from "../context/auth-context";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-
-
 const notesInitialState = {
   notes: [],
 };
@@ -13,68 +11,71 @@ const notesInitialState = {
 const NotesContext = createContext();
 
 const NotesProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [pinnedNotes, setPinnedNotes] = useState([]);
-  // const [unpinnedNotes, setUnpinnedNotes] = useState([]);
- 
-    const [notesState, notesDispatch] = useReducer(
+  const [notesState, notesDispatch] = useReducer(
     notesReducer,
     notesInitialState
   );
- const navigate = useNavigate();
+  const navigate = useNavigate();
   const { authState } = useAuth();
 
   const token = authState.token || JSON.parse(localStorage.getItem("token"));
 
-  const createNote = async (e,notesContent,setNoteContent) => {
-      e.preventDefault();
-      if(notesContent.title !== "" && notesContent.content !== ""){
-        try {
-          if(authState.token){
-              const response = await axios.post(
-                  "/api/notes",
-                  { note:notesContent },
-                  { headers: { authorization: token } }
-                );
-               console.log(response)
-                if(response.status === 201){
-                    localStorage.setItem("notes", JSON.stringify(response.data.notes));
-                    notesDispatch({type: "ADD_NOTE",  payload: response.data.notes});
-                    setNoteContent({title: "", content: ""})
-  
-                }else{
-                    throw new Error("Can't Process Request")
-                }
-          
-              }else{
-                  navigate('/')
-              }
+  const createNote = async (e, notesContent, setNoteContent) => {
+    e.preventDefault();
+    if (notesContent.title !== "" && notesContent.content !== "") {
+      try {
+        if (authState.token) {
+          const response = await axios.post(
+            "/api/notes",
+            { note: notesContent },
+            { headers: { authorization: token } }
+          );
+          console.log("in notes-context", response);
+          if (response.status === 201) {
+            localStorage.setItem("notes", JSON.stringify(response.data.notes));
+            notesDispatch({ type: "ADD_NOTE", payload: response.data.notes });
+            setNoteContent({ title: "", content: "" });
+            setIsOpen(false);
+          } else {
+            throw new Error("Can't Process Request");
           }
-         catch (error) {
+        } else {
+          navigate("/");
+        }
+      } catch (error) {
         console.log(error);
       }
-      }else{
-        alert("Title and content cannot be empty")
-      }
-    
-   
+    } else {
+      alert("Title and content cannot be empty");
+    }
   };
 
-  const getNotesData = async() =>{
+  const getNotesData = async () => {
     try {
-
-      const response = await axios.get("/api/notes",  { headers: { authorization: token } });
-      if(response.status === 200){
-        notesDispatch({type: "ADD_NOTES", payload: response.data.notes})
+      const response = await axios.get("/api/notes", {
+        headers: { authorization: token },
+      });
+      if (response.status === 200) {
+        notesDispatch({ type: "ADD_NOTES", payload: response.data.notes });
       }
-      
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <NotesContext.Provider value={{ notesState, notesDispatch, createNote, getNotesData,pinnedNotes, setPinnedNotes}}>
+    <NotesContext.Provider
+      value={{
+        notesState,
+        notesDispatch,
+        createNote,
+        getNotesData,
+        isOpen,
+        setIsOpen,
+      }}
+    >
       {children}
     </NotesContext.Provider>
   );
@@ -82,4 +83,4 @@ const NotesProvider = ({ children }) => {
 
 const useNote = () => useContext(NotesContext);
 
-export { NotesProvider, useNote }
+export { NotesProvider, useNote };
