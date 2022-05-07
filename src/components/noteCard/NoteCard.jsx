05b2a9cb111dfe59774ef83929f1555855e00e2c
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-07 18:23:39 +0530

    feat: Edit note functionality

--- src/components/noteCard/NoteCard.jsx
+++ src/components/noteCard/NoteCard.jsx
@@ -4,11 +4,14 @@ import { useState } from "react";
 
 import "./NoteCard.css";
 import toast from "react-hot-toast";
+import { useNote } from "../../context/notes-context";
+import { EditNoteModal } from "../modal/editNoteModal/EditNoteModal";
 
-
-const NoteCard = ({ notes, pinnedNotes, setPinnedNotes}) => {
+const NoteCard = ({ notes, pinnedNotes, setPinnedNotes }) => {
   const [isPinned, setIsPinned] = useState(false);
-  const { title, content, tags,color,priority } = notes;
+  const [editNoteVisible, setEditNoteVisible] = useState(false);
+
+  const { title, content, tags, color, priority } = notes;
 
   const [label] = tags;
 
@@ -34,50 +37,59 @@ const NoteCard = ({ notes, pinnedNotes, setPinnedNotes}) => {
     toast("Note Unpinned");
   };
 
- 
   return (
-    <div className="card notes-card card-with-dismiss" style={{backgroundColor: color}}>
-      <div className="card-header">
-        <div className="card-left">
-          <h5 className="card-title lg-text">{title}</h5>
-          <p className="card-text">{content}</p>
+    <>
+      <div
+        className="card notes-card card-with-dismiss"
+        style={{ backgroundColor: color }}
+      >
+        <div className="card-header">
+          <div className="card-left">
+            <h5 className="card-title lg-text">{title}</h5>
+            <p className="card-text">{content}</p>
+          </div>
+          <div className="card-right">
+            <button className="close-icon">
+              {isPinned ? (
+                <BsPinFill
+                  title="Pinned Notes"
+                  onClick={() => removePinnedNotes(notes)}
+                />
+              ) : (
+                <BsPin
+                  title="UnPinned Notes"
+                  onClick={() => addPinnedNotes(notes)}
+                />
+              )}
+            </button>
+          </div>
         </div>
-        <div className="card-right">
-          <button className="close-icon">
-            {isPinned ? (
-              <BsPinFill
-                title="Pinned Notes"
-                onClick={() => removePinnedNotes(notes)}
-              />
-            ) : (
-              <BsPin
-                title="UnPinned Notes"
-                onClick={() => addPinnedNotes(notes)}
-              />
-            )}
-          </button>
+        <div className="notes-label-priority md-text">
+          <div className="notes-features">{label}</div>
+          <div className="notes-features">{priority}</div>
+        </div>
+        <div className="note-footer">
+          <p className="note-date">Created on {date} </p>
+          <div className="note-action-btns">
+            <button className="action-btn">
+              <BsTrash />
+            </button>
+            <button
+              className="action-btn"
+              onClick={(e) => setEditNoteVisible(true)}
+            >
+              <MdOutlineModeEditOutline />
+            </button>
+            <button className="action-btn">
+              <MdOutlineArchive />
+            </button>
+          </div>
         </div>
       </div>
-      <div className="notes-label-priority md-text">
-        <div className="notes-features">{label}</div>
-        <div className="notes-features">{priority}</div>
-        
-        </div>
-      <div className="note-footer">
-        <p className="note-date">Created on {date} </p>
-        <div className="note-action-btns">
-          <button className="action-btn">
-            <BsTrash />
-          </button>
-          <button className="action-btn">
-            <MdOutlineModeEditOutline />
-          </button>
-          <button className="action-btn">
-            <MdOutlineArchive />
-          </button>
-        </div>
-      </div>
-    </div>
+      {editNoteVisible ? (
+        <EditNoteModal note={notes} setEditNoteVisible={setEditNoteVisible} />
+      ) : null}
+    </>
   );
 };
 
