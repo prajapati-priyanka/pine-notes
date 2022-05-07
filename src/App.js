69675a9508d9f5e4a2beffbc62b2d046fa1cee76
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-07 18:22:05 +0530

    fix: Remove nested routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import {Home, Landing, Login, Signup, Notes} from "./pages";
+import { Home, Landing, Login, Signup, Notes } from "./pages";
 import MockmanEs from "mockman-js";
 import { Toaster } from "react-hot-toast";
-
-
 
 function App() {
   return (
     <div className="App">
-     <Routes>
+      <Routes>
         <Route path="/" element={<Landing />} />
-       
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="mock" element={<MockmanEs />} />
-        <Route path="/home"
-            element={<Home />}>
-            <Route path="notes" element={<Notes />} />
-        </Route>
-     </Routes>
-     <Toaster
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/mock" element={<MockmanEs />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
+      </Routes>
+      <Toaster
         position="bottom-right"
         toastOptions={{ className: "showToast", duration: 3000 }}
       />
-     
     </div>
   );
 }
