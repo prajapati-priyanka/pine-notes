56b3fa51e366d95f53dd631fff299dcae10db1f9
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-07 18:23:01 +0530

    feat: Add new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
-export {CreateNote} from "./createNote/CreateNote";
-export {Filter} from "./filter/Filter";
-export{EditLable} from "./modal/EditLabel";
-export {NoteCard} from "./noteCard/NoteCard";
-export {SideNav} from "./sideNav/SideNav";
-export {PinnedCard} from "./pinnedCard/PinnedCard";
-export {ColorPallete} from "./colorPallete/ColorPallete";
+// export {CreateNote} from "./createNote/CreateNote";
+export { Filter } from "./filter/Filter";
+export { EditLable } from "./modal/editLabel/EditLabel";
+export { NoteCard } from "./noteCard/NoteCard";
+export { SideNav } from "./sideNav/SideNav";
+export { PinnedCard } from "./pinnedCard/PinnedCard";
+export { ColorPallete } from "./colorPallete/ColorPallete";
+export { CreateNoteModal } from "./modal/createNoteModal/CreateNoteModal";
+export { EditNoteModal } from "./modal/editNoteModal/EditNoteModal";
