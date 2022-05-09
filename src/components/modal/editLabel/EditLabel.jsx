f611602a2c4acf53328bf62f83ce45c7d47f9b15
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-10 02:09:26 +0530

    feat: Add Label functionality

--- src/components/modal/editLabel/EditLabel.jsx
+++ src/components/modal/editLabel/EditLabel.jsx
@@ -1,41 +1,44 @@
 import { BsCheckLg, BsXLg } from "react-icons/bs";
+import { useState } from "react";
+import { useLabels } from "../../../context/labels-context";
 import "./EditLabel.css";
 
-const EditLable = ({ setLabelModalVisible }) => {
+const EditLable = ({setLabelModalVisible}) => {
+
+  const {labelsState, labelsDispatch} = useLabels();
+ const [inputLabel, setInputLabel] = useState('')
+
   return (
     <div className="modal-background">
       <div className="modal-container">
         <h4 className="modal-title">Edit Labels</h4>
         <div className="input-container">
-          <input type="text" placeholder="Enter Label.." />
-          <button className="btn btn-check">
+          <input 
+          type="text" 
+          placeholder="Enter Label.." 
+          name="input-label"
+          value={inputLabel}
+          onChange = {(e)=> setInputLabel(e.target.value)}
+          />
+          <button className="btn btn-check" onClick={()=>labelsDispatch({type:"ADD_LABELS", payload: inputLabel})}>
             <BsCheckLg />
           </button>
         </div>
         <ul className="modal-lists">
-          <li>
-            <h4>Work</h4>
+          {labelsState.labels.map((label,index )=>(
+            <li key={index}>
+               <h4>{label}</h4>
             <button className="btn btn-check">
               <BsXLg />
             </button>
-          </li>
-          <li>
-            <h4>Home</h4>
-            <button className="btn btn-check">
-              <BsXLg />
-            </button>
-          </li>
-          <li>
-            <h4>Office</h4>
-            <button className="btn btn-check">
-              <BsXLg />
-            </button>
-          </li>
+            </li>
+          ))}
+         
         </ul>
         <div className="modal-btns">
           <button
             className="btn btn-outline-primary"
-            onClick={() => setLabelModalVisible(false)}
+            onClick={()=> setLabelModalVisible(false)}
           >
             CLOSE
           </button>
