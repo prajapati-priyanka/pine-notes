584f45782e1adea6bf4e73541bd3e7c8b7ffbedb
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-09 22:56:27 +0530

    feat: Add TagProvider and useTag

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,4 @@
 export {AuthProvider, useAuth} from "./auth-context";
 export {NotesProvider, useNote} from "./notes-context";
-export {TrashProvider, useTrash} from "./trash-context"
+export {TrashProvider, useTrash} from "./trash-context";
+export {TagsProvider, useTags} from "./tags-context";
