86b6925f3c9cc1848c716b0b7c2b51e616f33e4e
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-09 16:12:55 +0530

    feat: Add Toast

--- src/context/trash-context.js
+++ src/context/trash-context.js
@@ -1,112 +1,137 @@
-import { createContext,useContext,useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 import { trashReducer } from "../reducer/trash-reducer";
 import { useAuth } from "./auth-context";
 import axios from "axios";
 import { useNote } from "./notes-context";
+import toast from "react-hot-toast";
 
 
-const trashInitialState  ={
-    trash:[]
-}
-
+const trashInitialState = {
+  trash: [],
+};
 
 const TrashContext = createContext();
 
+const TrashProvider = ({ children }) => {
+  const [trashState, trashDispatch] = useReducer(
+    trashReducer,
+    trashInitialState
+  );
 
-const TrashProvider = ({children}) =>{
+  const { authState } = useAuth();
+  const { notesDispatch } = useNote();
+  const token = authState.token || localStorage.getItem("token");
 
-const [trashState, trashDispatch] = useReducer(trashReducer, trashInitialState);
+  // get all trashed notes
 
-const {authState} = useAuth();
-const {notesDispatch} = useNote();
-const token = authState.token || localStorage.getItem("token");
-
-
-const getAllTrashNotesHandler = async() =>{
+  const getAllTrashNotesHandler = async () => {
     try {
-        const response = await axios({
-            method: "GET",
-            url: `/api/trash`,
-            headers: { authorization: token },
-          });
-      
-        if(response === 200){
-            trashDispatch({type:"UPDATE_TRASH", payload: response.data.trash})
-        }else{
-            throw new Error("Can't process the request")
-        }
+      const response = await axios({
+        method: "GET",
+        url: `/api/trash`,
+        headers: { authorization: token },
+      });
+
+      if (response === 200) {
+        trashDispatch({ type: "UPDATE_TRASH", payload: response.data.trash });
+      } else {
+        throw new Error("Can't process the request");
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  };
 
-const moveToTrashHandler = async (notes) =>{
+  // move notes to trash
 
+  const moveToTrashHandler = async (notes) => {
     try {
-        const response = await axios({
-            method: "POST",
-            url: `/api/notes/trash/${notes._id}`,
-            headers: { authorization: token },
-          });
-      
+      const response = await axios({
+        method: "POST",
+        url: `/api/notes/trash/${notes._id}`,
+        headers: { authorization: token },
+      });
 
-        console.log(response)
-        if(response.status === 201){
-           trashDispatch ({type: "MOVE_TO_TRASH", payload: response.data.trash});
-           notesDispatch({type: "DELETE_FROM_NOTE", payload: response.data.notes})
-        }else{
-            throw new Error("Can't process the request")
-        }
-        
+      if (response.status === 201) {
+        trashDispatch({ type: "MOVE_TO_TRASH", payload: response.data.trash });
+        notesDispatch({
+          type: "DELETE_FROM_NOTE",
+          payload: response.data.notes,
+        });
+        toast("Note is moved to trash", { icon: "✔️" });
+      } else {
+        throw new Error("Can't process the request");
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  };
 
-const restoreFromTrashHandler = async(notes)=>{
+  //   restore notes from trash
+
+  const restoreFromTrashHandler = async (notes) => {
     try {
-        const response = await axios({
-            method: "POST",
-            url: `/api/trash/restore/${notes._id}`,
-            headers: { authorization: token },
-          });
-     
-          if(response.status === 200){
-              trashDispatch({type: "RESTORE_FROM_TRASH", payload: response.data.trash});
-              notesDispatch({type: "UPDATE_NOTES", payload: response.data.notes});
-          }else{
-            throw new Error("Can't process the request")
-        }
+      const response = await axios({
+        method: "POST",
+        url: `/api/trash/restore/${notes._id}`,
+        headers: { authorization: token },
+      });
 
+      if (response.status === 200) {
+        trashDispatch({
+          type: "RESTORE_FROM_TRASH",
+          payload: response.data.trash,
+        });
+        notesDispatch({ type: "UPDATE_NOTES", payload: response.data.notes });
+        toast("Note restored from trash", { icon: "✔️" });
+      } else {
+        throw new Error("Can't process the request");
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  };
 
-const deleteFromTrashHandler = async (notes) =>{
+  //  delete notes from trash
+
+  const deleteFromTrashHandler = async (notes) => {
     try {
-        const response = await axios({
-            method: "DELETE",
-            url: `/api/trash/delete/${notes._id}`,
-            headers: { authorization: token },
-          });
-        
-          console.log("InDeketefromtrashHandler", response)
-          if(response.status === 200){
-             trashDispatch({type: "DELETE_FROM_TRASH", payload: response.data.trash});
-          }else{
-            throw new Error("Can't process the request")
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
+      const response = await axios({
+        method: "DELETE",
+        url: `/api/trash/delete/${notes._id}`,
+        headers: { authorization: token },
+      });
 
-    return <TrashContext.Provider value={{trashState, trashDispatch,getAllTrashNotesHandler,  moveToTrashHandler, restoreFromTrashHandler,deleteFromTrashHandler}}>
-        {children}
+      if (response.status === 200) {
+        trashDispatch({
+          type: "DELETE_FROM_TRASH",
+          payload: response.data.trash,
+        });
+        toast("Note is deleted from trash", { icon: "✔️" });
+      } else {
+        throw new Error("Can't process the request");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <TrashContext.Provider
+      value={{
+        trashState,
+        trashDispatch,
+        getAllTrashNotesHandler,
+        moveToTrashHandler,
+        restoreFromTrashHandler,
+        deleteFromTrashHandler,
+      }}
+    >
+      {children}
     </TrashContext.Provider>
-}
+  );
+};
 
 const useTrash = () => useContext(TrashContext);
 
-export{TrashProvider, useTrash}
+export { TrashProvider, useTrash };
