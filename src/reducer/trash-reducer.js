694d9da1e7b1c99ea920c0b62623ad16d84c63e8
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-09 14:20:12 +0530

    feat: Add cases for different network calls

--- src/reducer/trash-reducer.js
+++ src/reducer/trash-reducer.js
@@ -1,11 +1,31 @@
-const trashReducer = (state,action) =>{
-    switch(action.type){
-       case "UPDATE_TRASH":{
-           return {
-               ...state,
-               trash: action.payload
-           }
-       }
+const trashReducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_TRASH": {
+      return {
+        ...state,
+        trash: action.payload,
+      };
     }
-}
-export {trashReducer}
+    case "MOVE_TO_TRASH": {
+      return {
+        ...state,
+        trash: action.payload,
+      };
+    }
+    case "RESTORE_FROM_TRASH": {
+      return {
+        ...state,
+        trash: action.payload,
+      };
+    }
+    case "DELETE_FROM_TRASH": {
+      return {
+        ...state,
+        trash: action.payload,
+      };
+    }
+    default:
+      return state;
+  }
+};
+export { trashReducer };
