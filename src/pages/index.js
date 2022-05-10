d12990350613ec56b42fcd35e69df32ce4e6e041
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-10 16:32:50 +0530

    feat: Add Archive page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,5 @@ export {Login} from "./authentication/login/Login";
 export {Signup} from "./authentication/signup/Signup";
 export {Landing} from "./landing/Landing";
 export {Notes} from "./notes/Notes";
-export {Trash} from "./trash/Trash"
+export {Trash} from "./trash/Trash";
+export {Archive} from "./archive/Archive";
