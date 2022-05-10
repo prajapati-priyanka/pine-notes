7ac6a1e78b36fecf6c1de9a8ed52468e644a5fe0
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-10 12:27:12 +0530

    feat: Add delete label functionality

--- src/components/modal/editLabel/EditLabel.jsx
+++ src/components/modal/editLabel/EditLabel.jsx
@@ -6,11 +6,19 @@ import "./EditLabel.css";
 const EditLable = ({setLabelModalVisible}) => {
 
   const {labelsState, labelsDispatch} = useLabels();
- const [inputLabel, setInputLabel] = useState('')
+ const [inputLabel, setInputLabel] = useState('');
+
+ const deleteLabelHandler = (label)=>{
+   
+   const labelToDelete = labelsState.labels.filter(item => item !== label) ;
+  
+   labelsDispatch({type: "DELETE_LABELS", payload: labelToDelete})
+
+ }
 
   return (
     <div className="modal-background">
-      <div className="modal-container">
+      <div className="modal-container label-modal">
         <h4 className="modal-title">Edit Labels</h4>
         <div className="input-container">
           <input 
@@ -20,16 +28,23 @@ const EditLable = ({setLabelModalVisible}) => {
           value={inputLabel}
           onChange = {(e)=> setInputLabel(e.target.value)}
           />
-          <button className="btn btn-check" onClick={()=>labelsDispatch({type:"ADD_LABELS", payload: inputLabel})}>
+          <button 
+          disabled= {inputLabel.length < 1 || inputLabel.length >8}
+          className="btn btn-check" 
+          onClick={()=>{
+            labelsDispatch({type:"ADD_LABELS", payload: inputLabel});
+            setInputLabel("")
+            }}>
             <BsCheckLg />
           </button>
         </div>
+        {(inputLabel.length < 0 || inputLabel.length >8) ? (<div className="error-msg md-text">Maximum Length: 8</div>) : null}
         <ul className="modal-lists">
           {labelsState.labels.map((label,index )=>(
             <li key={index}>
                <h4>{label}</h4>
             <button className="btn btn-check">
-              <BsXLg />
+              <BsXLg onClick={()=>deleteLabelHandler(label)}/>
             </button>
             </li>
           ))}
