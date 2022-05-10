f82d842be746a7bcf54263b404017aefb3d7e95b
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-10 16:32:10 +0530

    feat: Add ArchiveProvider and useArchive

--- src/context/index.js
+++ src/context/index.js
@@ -2,4 +2,4 @@ export {AuthProvider, useAuth} from "./auth-context";
 export {NotesProvider, useNote} from "./notes-context";
 export {TrashProvider, useTrash} from "./trash-context";
 export {LabelsProvider, useLabels} from "./labels-context";
-export {ArchieveProvider, useArchieve} from "./archieve-context";
+export {ArchiveProvider, useArchive} from "./archive-context";
