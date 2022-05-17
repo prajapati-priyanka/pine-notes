25b36f24471b91e6912d570051846564df998b7a
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-17 15:02:00 +0530

    refactor: Move network calls to helpers & services

--- src/context/notes-context.js
+++ src/context/notes-context.js
@@ -3,7 +3,7 @@ import { notesReducer } from "../reducer/notes-reducer";
 import { useAuth } from "../context/auth-context";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
+
 
 const notesInitialState = {
   notes: [],
@@ -13,13 +13,12 @@ const NotesContext = createContext();
 
 const NotesProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-
+  
   const [notesState, notesDispatch] = useReducer(
     notesReducer,
     notesInitialState
   );
-  const navigate = useNavigate();
+
   const { authState } = useAuth();
 
   const token = authState.token || JSON.parse(localStorage.getItem("token"));
@@ -40,41 +39,6 @@ const getAllNotesHandler = async()=>{
 }
 
 
-  const createNote = async (
-    e,
-    notesContent,
-    setNoteContent,
-  ) => {
-    e.preventDefault();
-    if (notesContent.title !== "" && notesContent.content !== "") {
-      try {
-        if (authState.token) {
-          const response = await axios.post(
-            "/api/notes",
-            { note: notesContent },
-            { headers: { authorization: token } }
-          );
-         
-          if (response.status === 201) {
-            localStorage.setItem("notes", JSON.stringify(response.data.notes));
-            notesDispatch({ type: "ADD_NOTE", payload: response.data.notes });
-            setNoteContent({ title: "", content: "", priority: "" });
-            setIsOpen(false);
-            toast("New note added succesfully", { icon: "✔️" });
-          } else {
-            throw new Error("Can't Process Request");
-          }
-        } else {
-          navigate("/");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      alert("Title and content cannot be empty");
-    }
-  };
-
   const getNotesData = async () => {
     try {
       const response = await axios.get("/api/notes", {
@@ -90,43 +54,16 @@ const getAllNotesHandler = async()=>{
     }
   };
 
-  const editNote = async (e, notesContent, setNoteContent) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        `/api/notes/${notesContent._id}`,
-        { note: notesContent },
-        {
-          headers: { authorization: token },
-        }
-      );
-      if (response.status === 201) {
-        notesDispatch({ type: "ADD_NOTE", payload: response.data.notes });
-        setNoteContent({ title: "", content: "", priority: "" });
-        setIsOpen(false);
-        setIsEditing(false);
-        toast("Note edited successfully", { icon: "✔️" });
-      } else {
-        throw new Error("Can't Process Request");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <NotesContext.Provider
       value={{
         notesState,
         notesDispatch,
-        createNote,
         getNotesData,
         isOpen,
         setIsOpen,
-        isEditing,
-        setIsEditing,
-        editNote,
-        getAllNotesHandler
+       getAllNotesHandler
       }}
     >
       {children}
