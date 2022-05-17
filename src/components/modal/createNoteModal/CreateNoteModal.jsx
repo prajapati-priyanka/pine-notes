01d29cc7e3dc989d51668f7f5791e3476ed5200a
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-17 23:37:16 +0530

    fix: Delete choose option

--- src/components/modal/createNoteModal/CreateNoteModal.jsx
+++ src/components/modal/createNoteModal/CreateNoteModal.jsx
@@ -140,9 +140,7 @@ const CreateNoteModal = ({
                     }))
                   }
                 >
-                  <option value="">--Choose--</option>
                   <option value="high">High</option>
-                  <option value="medium">Medium</option>
                   <option value="low">Low</option>
                 </select>
                 <div>
