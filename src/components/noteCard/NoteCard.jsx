4c0fd49591ad66c6070e3e783161122ffdfd1198
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-17 14:58:39 +0530

    feat: Add date, time ,editNoteHandler

--- src/components/noteCard/NoteCard.jsx
+++ src/components/noteCard/NoteCard.jsx
@@ -3,37 +3,37 @@ import {
   MdOutlineModeEditOutline,
   MdOutlineArchive,
   MdRestoreFromTrash,
-  MdOutlineUnarchive
+  MdOutlineUnarchive,
 } from "react-icons/md";
 import { useState } from "react";
 import "./NoteCard.css";
 import toast from "react-hot-toast";
-import { EditNoteModal } from "../modal/editNoteModal/EditNoteModal";
 import { useLocation } from "react-router-dom";
 import { useTrash, useArchive } from "../../context";
 import { getDateString, getTimeString } from "../../helpers/notesHelpers";
 
-const NoteCard = ({ notes, pinnedNotes, setPinnedNotes }) => {
+const NoteCard = ({
+  notes,
+  pinnedNotes,
+  setPinnedNotes,
+  setEditNote,
+  setCreateNoteModalVisible,
+}) => {
   const [isPinned, setIsPinned] = useState(false);
-  const [editNoteVisible, setEditNoteVisible] = useState(false);
   const location = useLocation();
   const {
     moveToTrashHandler,
     restoreFromTrashHandler,
     deleteFromTrashHandler,
   } = useTrash();
- 
+
   const {
     moveToArchiveHandler,
     restoreFromArchiveHandler,
-    deleteFromArchiveHandler
-     } = useArchive();
+    deleteFromArchiveHandler,
+  } = useArchive();
 
-  const { title, content, tags, color, priority, date } = notes;
-
-  const [label] = tags;
-
-
+  const { title, content, labels, color, priority, date } = notes;
 
   const addPinnedNotes = (note) => {
     const newNote = pinnedNotes.find((item) => item._id === note._id);
@@ -56,105 +56,103 @@ const NoteCard = ({ notes, pinnedNotes, setPinnedNotes }) => {
   };
 
   const moveToTrash = (notes) => {
-  
     if (location.pathname === "/home") {
       moveToTrashHandler(notes);
     }
     if (location.pathname === "/trash") {
       deleteFromTrashHandler(notes);
     }
-    if (location.pathname === "/archive"){
+    if (location.pathname === "/archive") {
       deleteFromArchiveHandler(notes);
     }
-   
   };
- 
 
- 
+  const editNoteHandler = () => {
+    setEditNote(notes);
+    setCreateNoteModalVisible(true);
+  };
 
   return (
-    <>
-      <div
-        className="card notes-card card-with-dismiss"
-        style={{ backgroundColor: color }}
-      >
-        <div className="card-header">
-          <div className="card-left">
-            <h5 className="card-title lg-text">{title}</h5>
-            <p className="card-text">{content}</p>
-          </div>
-          <div className="card-right">
-            <button className="close-icon">
-              {isPinned ? (
-                <BsPinFill
-                  title="Pinned Notes"
-                  onClick={() => removePinnedNotes(notes)}
-                />
-              ) : (
-                <BsPin
-                  title="UnPinned Notes"
-                  onClick={() => addPinnedNotes(notes)}
-                />
-              )}
-            </button>
-          </div>
+    <div
+      className="card notes-card card-with-dismiss"
+      style={{ backgroundColor: color }}
+    >
+      <div className="card-header">
+        <div className="card-left">
+          <h5 className="card-title lg-text">{title}</h5>
+          <p className="card-text">{content}</p>
         </div>
-        <div className="notes-label-priority md-text">
-       {label && <div className="notes-features">{label}</div>}   
-         {priority && <div className="notes-features">{priority}</div>}
+        <div className="card-right">
+          <button className="close-icon">
+            {isPinned ? (
+              <BsPinFill
+                title="Pinned Notes"
+                onClick={() => removePinnedNotes(notes)}
+              />
+            ) : (
+              <BsPin
+                title="UnPinned Notes"
+                onClick={() => addPinnedNotes(notes)}
+              />
+            )}
+          </button>
         </div>
-        <div className="note-footer">
-          <p className="note-date">{getDateString(date)} | {getTimeString(date)} </p>
-          <div className="note-action-btns">
+      </div>
+      <div className="notes-label-priority md-text">
+        {labels && <div className="notes-features">{labels}</div>}
+        {priority && <div className="notes-features">{priority}</div>}
+      </div>
+      <div className="note-footer">
+        <p className="note-date">
+          {getDateString(date)} | {getTimeString(date)}{" "}
+        </p>
+        <div className="note-action-btns">
+          <button
+            className="action-btn"
+            title="delete"
+            onClick={() => moveToTrash(notes)}
+          >
+            <BsTrash />
+          </button>
 
-        
-            <button
-              className="action-btn"
-              title="delete"
-              onClick={() => moveToTrash(notes)}
-            >
-              <BsTrash />
-            </button>
-             
-
-            {location.pathname === "/home"  ? (<>
-              <button
-                className="action-btn"
-                onClick={(e) => setEditNoteVisible(true)}
-              >
+          {location.pathname === "/home" ? (
+            <>
+              <button className="action-btn" onClick={editNoteHandler}>
                 <MdOutlineModeEditOutline />
               </button>
 
-           <button className="action-btn" title="archive" onClick={()=> moveToArchiveHandler(notes)}>
-            <MdOutlineArchive />
-            </button>
-            </>
-            ): null }
-
-            {location.pathname === "/trash" &&  (
               <button
                 className="action-btn"
-                title="restore"
-                onClick={() => restoreFromTrashHandler(notes)}
+                title="archive"
+                onClick={() => moveToArchiveHandler(notes)}
               >
-                <MdRestoreFromTrash />
+                <MdOutlineArchive />
               </button>
-            ) 
-          
-            }
+            </>
+          ) : null}
 
-            {location.pathname === "/archive" && (
-              <button className="action-btn" title="unarchive" onClick={()=>restoreFromArchiveHandler(notes)}>
-                 <MdOutlineUnarchive/>
-                 </button>
-            )}
-          </div>
+          {location.pathname === "/trash" && (
+            <button
+              className="action-btn"
+              title="restore"
+              onClick={() => restoreFromTrashHandler(notes)}
+            >
+              <MdRestoreFromTrash />
+            </button>
+          )}
+
+          {location.pathname === "/archive" && (
+            <button
+              className="action-btn"
+              title="unarchive"
+              onClick={() => restoreFromArchiveHandler(notes)}
+            >
+              <MdOutlineUnarchive />
+            </button>
+          )}
         </div>
       </div>
-      {editNoteVisible ? (
-        <EditNoteModal note={notes} setEditNoteVisible={setEditNoteVisible} />
-      ) : null}
-    </>
+    </div>
   );
 };
 
