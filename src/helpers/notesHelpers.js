8c0092f950d035f4e837a6de265075f7985f134f
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-28 16:33:32 +0530

    feat: Add function to convertFirstLetterCapital

--- src/helpers/notesHelpers.js
+++ src/helpers/notesHelpers.js
@@ -2,4 +2,9 @@ const getDateString = date => date.slice(0,9)
 
 const getTimeString = date => date.slice(10,15);
 
-export {getDateString, getTimeString}
+const convertStringFirstLetterCapital = (str) =>{
+    const remainingChar = str.slice(1);
+    return str.charAt(0).toUpperCase() + remainingChar;
+}
+
+export {getDateString, getTimeString, convertStringFirstLetterCapital}
