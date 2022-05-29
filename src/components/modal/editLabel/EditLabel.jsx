79b91ab0c1540bbf53a5294030586f47dda3d86d
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-29 21:00:26 +0530

    refactor: Done format

--- src/components/modal/editLabel/EditLabel.jsx
+++ src/components/modal/editLabel/EditLabel.jsx
@@ -4,57 +4,56 @@ import { useLabels } from "../../../context/labels-context";
 import "./EditLabel.css";
 import { convertStringFirstLetterCapital } from "../../../helpers/notesHelpers";
 
-const EditLable = ({setLabelModalVisible}) => {
+const EditLable = ({ setLabelModalVisible }) => {
+  const { labelsState, labelsDispatch } = useLabels();
+  const [inputLabel, setInputLabel] = useState("");
 
-  const {labelsState, labelsDispatch} = useLabels();
- const [inputLabel, setInputLabel] = useState('');
+  const deleteLabelHandler = (label) => {
+    const labelToDelete = labelsState.labels.filter((item) => item !== label);
 
- const deleteLabelHandler = (label)=>{
-   
-   const labelToDelete = labelsState.labels.filter(item => item !== label) ;
-  
-   labelsDispatch({type: "DELETE_LABELS", payload: labelToDelete})
-
- }
+    labelsDispatch({ type: "DELETE_LABELS", payload: labelToDelete });
+  };
 
   return (
     <div className="modal-background">
       <div className="modal-container label-modal">
         <h4 className="modal-title">Edit Labels</h4>
         <div className="input-container">
-          <input 
-          type="text" 
-          placeholder="Enter Label.." 
-          name="input-label"
-          value={inputLabel}
-          onChange = {(e)=> setInputLabel(e.target.value)}
+          <input
+            type="text"
+            placeholder="Enter Label.."
+            name="input-label"
+            value={inputLabel}
+            onChange={(e) => setInputLabel(e.target.value)}
           />
-          <button 
-          disabled= {inputLabel.length < 1 || inputLabel.length >8}
-          className="btn btn-check" 
-          onClick={()=>{
-            labelsDispatch({type:"ADD_LABELS", payload: inputLabel});
-            setInputLabel("")
-            }}>
+          <button
+            disabled={inputLabel.length < 1 || inputLabel.length > 8}
+            className="btn btn-check"
+            onClick={() => {
+              labelsDispatch({ type: "ADD_LABELS", payload: inputLabel });
+              setInputLabel("");
+            }}
+          >
             <BsCheckLg />
           </button>
         </div>
-        {(inputLabel.length < 0 || inputLabel.length >8) ? (<div className="error-msg md-text">Maximum Length: 8</div>) : null}
+        {inputLabel.length < 0 || inputLabel.length > 8 ? (
+          <div className="error-msg md-text">Maximum Length: 8</div>
+        ) : null}
         <ul className="modal-lists">
-          {labelsState.labels.map((label,index )=>(
+          {labelsState.labels.map((label, index) => (
             <li key={index}>
-               <h4>{convertStringFirstLetterCapital(label)}</h4>
-            <button className="btn btn-check">
-              <BsXLg onClick={()=>deleteLabelHandler(label)}/>
-            </button>
+              <h4>{convertStringFirstLetterCapital(label)}</h4>
+              <button className="btn btn-check">
+                <BsXLg onClick={() => deleteLabelHandler(label)} />
+              </button>
             </li>
           ))}
-         
         </ul>
         <div className="modal-btns">
           <button
             className="btn btn-outline-primary"
-            onClick={()=> setLabelModalVisible(false)}
+            onClick={() => setLabelModalVisible(false)}
           >
             CLOSE
           </button>
